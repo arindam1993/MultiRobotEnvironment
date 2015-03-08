549be0e7da59f69910d4711d991f9a0707d7b86b
Chris Chow <chris.kr.chow@gmail.com>
2015-03-07 21:06:30 -0500

    faster video rendering

--- Simulator.py
+++ Simulator.py
@@ -5,6 +5,8 @@ Created on Jan 16, 2015
 '''
 
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from numpy import *
 from World import *
@@ -15,7 +17,6 @@ from Ball import Ball
 from LinearAlegebraUtils import distBetween
 from RunAtBallBrain import RunAtBallBrain
 from Team import Team
-
 
 
 #Called once for initialization
@@ -33,6 +34,13 @@ class Simulator(object):
         self.imageDirName = imageDirName
         self.currWP = 0
         self.ballWPs = [array([50.0, -100.0, 0.0]), array([0.0, 100.0, -70.0]), array([50.0, 20.0, 100.0]),array([-30.0, 50.0, -100.0]), array([80.0, -50.0, 50.0]), array([80.0, -50.0, -50.0]), array([-65.0, 20.0, 50.0]), array([-50.0, 20.0, -60.0])]
+        self.fig = plt.figure(figsize=(16,12))
+        self.timeStep = 1/float(30)
+        self.frameProb = float(self.fps) / 30
+        self.currTime = float(0)
+        self.drawIndex = 0
+        self.physicsIndex = 0
+
 
     def setup(self):    
         #setup directory to save the images
@@ -74,7 +82,6 @@ class Simulator(object):
         self.world.agents.append(agent3)
         self.world.agents.append(agent4)
 
-#         
         #define a bunch of obstacles
         ob1Pos = array([-50,-50,-50])
         ob1 = Obstacle(ob1Pos, 30)
@@ -83,7 +90,7 @@ class Simulator(object):
         ob2 = Obstacle(ob2Pos, 20)
          
         #add obstacles to the world
-        self.world.obstacles.append(ob1);
+        self.world.obstacles.append(ob1)
         self.world.obstacles.append(ob2)
         
         #define a ball
@@ -93,7 +100,8 @@ class Simulator(object):
         #add the ball to the world
         self.world.balls.append(ball)
         
-#called at a fixed 30fps always
+    # called at a fixed 30fps always
+    # physics simulation
     def fixedLoop(self):
         for agent in self.world.agents:
             agent.moveAgent(self.world)
@@ -106,55 +114,61 @@ class Simulator(object):
                         self.ballWPs.remove(self.ballWPs[0])
 
     
-#Called at specifed fps
+    #Called at specifed fps
     def loop(self, ax):       
         self.world.draw(ax)
-       
-                
-    def run(self):
-        #Run setup once
-        self.setup()
-        
-        #Setup loop
-        timeStep = 1/double(30)
-        frameProb = double(self.fps) / 30
-        currTime = double(0)
-        drawIndex = 0
-        physicsIndex = 0
-        while(currTime < self.simTime):
-            self.fixedLoop()
-            currProb = double(drawIndex)/double(physicsIndex+1)
-            if currProb < frameProb:
-                self.drawFrame(drawIndex)  
-                drawIndex+=1
-            physicsIndex+=1
-            currTime+=double(timeStep)
-     
-        print "Physics ran for "+str(physicsIndex)+" steps"
-        print "Drawing ran for "+str(drawIndex)+" steps"
-            
-    def drawFrame(self, loopIndex):
-        fig = plt.figure(figsize=(16,12))
-        ax = fig.add_subplot(111, projection='3d') 
+
+    # advances simulation 1 tick, returns the current world state plot
+    def tick(self):
+        self.fixedLoop()
+
+        self.fig.clf()
+        ax = self.fig.add_subplot(111, projection='3d')
         ax.view_init(elev = 30)
         ax.set_xlabel("X")
         ax.set_ylabel("Y")
-        ax.set_zlabel("Z")    
-        fname = self.imageDirName + '/' + str(int(100000000+loopIndex)) + '.png' # name the file 
+        ax.set_zlabel("Z")
+
+        return self.loop(ax), plt.gca().set_ylim(ax.get_ylim()[::-1])
+
+    def run(self):
+        #Run setup once
+        self.setup()
+
+        while(self.currTime < self.simTime):
+            self.fixedLoop()
+            currProb = float(self.drawIndex)/float(self.physicsIndex+1)
+            if currProb < self.frameProb:
+                self.drawFrame(self.drawIndex)
+                self.drawIndex+=1
+            self.physicsIndex+=1
+            self.currTime+=float(self.timeStep)
+     
+        print "Physics ran for "+str(self.physicsIndex)+" steps"
+        print "Drawing ran for "+str(self.drawIndex)+" steps"
+            
+    def drawFrame(self, loopIndex):
+        self.fig.clf()
+        ax = self.fig.add_subplot(111, projection='3d')
+        ax.view_init(elev = 30)
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
+        fname = self.imageDirName + "/img{:08d}.png".format(int(loopIndex)) # name the file
         self.loop(ax)
         plt.gca().set_ylim(ax.get_ylim()[::-1])
-        savefig(fname, format='png', bbox_inches='tight')
+        self.fig.savefig(fname, format='png', bbox_inches='tight')
         print 'Written Frame No.'+ str(loopIndex)+' to '+ fname
-        plt.close()
+
 
 
 #Simulation runs here
 #set the size of the world
-world = World(100, 100)
-#specify which world to simulate, total simulation time, and frammerate for video
-sim = Simulator(world, 60, 30, "images")
+# world = World(100, 100)
+#specify which world to simulate, total simulation time, and framerate for video
+# sim = Simulator(world, 60, 30, "images")
 #run the simulation
-sim.run()
+# sim.run()
 
 '''
 To create a video using the image sequence, execute the following command in command line.
